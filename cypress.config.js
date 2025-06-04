228bfaf6a4df27e83a24a91ac022714d80391188
Fermoal <105529956+Fermoal@users.noreply.github.com>
2025-06-03 18:56:09 -0600

    implementing mochawesome reporting toll, 14 test passed on run, reporint tool generates ahtml report UI for user

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,18 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      
     },
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: false,
+      json: true
+    }
   },
+
+  setupNodeEvents(on, config) {
+    
+  }
 });
